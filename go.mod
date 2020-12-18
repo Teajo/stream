f2e1c60b88c588f3ad182422d266dda880d77167
Joffrey <joffrey.perrin-bit@planete-oui.fr>
2020-12-18 13:05:42 +0100

    pass infos to env

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module stream
 go 1.15
 
 require (
+	github.com/NYTimes/gziphandler v1.1.1
+	github.com/giorgisio/goav v0.1.0
 	github.com/google/uuid v1.1.2
 	github.com/gorilla/mux v1.8.0
 )
--- main.go
+++ main.go
@@ -16,16 +16,27 @@ import (
 var folder string = "chunks"
 
 func main() {
-	args := os.Args[1:]
-	if len(args) == 1 {
-		fmt.Println("Reuse previous chunks")
-	} else if len(args) < 4 {
-		panic("Must provide video file, resolution and time chunk as arguments")
-	} else {
-		file := args[1]
-		resolution := args[2]
-		timechunk := args[3]
+	port, err := getEnvPort()
+	if err != nil {
+		panic(err)
+	}
 
+	file, err := getEnvFile()
+	if err != nil {
+		panic(err)
+	}
+
+	resolution, err := getEnvResolution()
+	if err != nil {
+		panic(err)
+	}
+
+	timechunk, err := getEnvChunk()
+	if err != nil {
+		panic(err)
+	}
+
+	if file != "" {
 		fmt.Println("Clean previous chunks")
 		os.Remove(folder)
 		os.Mkdir(folder, os.ModePerm)
@@ -42,21 +53,16 @@ func main() {
 		}()
 	}
 
-	port := args[0]
 	http.Handle("/", handlers())
-	fmt.Println(fmt.Sprintf("Listen at 127.0.0.1:%s", port))
+	fmt.Println(fmt.Sprintf("Listen at :%s", port))
 	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
-}
-
-func process() {
-
 }
 
 func handlers() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/", indexPage).Methods("GET")
-	router.HandleFunc("/media/{mId:[0-9]+}/stream/", streamHandler).Methods("GET")
-	router.HandleFunc("/media/{mId:[0-9]+}/stream/{segName:index[0-9]+.ts}", streamHandler).Methods("GET")
+	router.Handle("/media/{mId:[0-9]+}/stream/", http.HandlerFunc(streamHandler)).Methods("GET")
+	router.Handle("/media/{mId:[0-9]+}/stream/{segName:index[0-9]+.ts}", http.HandlerFunc(streamHandler)).Methods("GET")
 	return router
 }
 
@@ -66,7 +72,7 @@ func indexPage(w http.ResponseWriter, r *http.Request) {
 
 func streamHandler(response http.ResponseWriter, request *http.Request) {
 	vars := mux.Vars(request)
-	mId, err := strconv.Atoi(vars["mId"])
+	mID, err := strconv.Atoi(vars["mId"])
 	if err != nil {
 		response.WriteHeader(http.StatusNotFound)
 		return
@@ -74,17 +80,17 @@ func streamHandler(response http.ResponseWriter, request *http.Request) {
 
 	segName, ok := vars["segName"]
 	if !ok {
-		mediaBase := getMediaBase(mId)
+		mediaBase := getMediaBase(mID)
 		m3u8Name := "index.m3u8"
 		serveHlsM3u8(response, request, mediaBase, m3u8Name)
 	} else {
-		mediaBase := getMediaBase(mId)
+		mediaBase := getMediaBase(mID)
 		serveHlsTs(response, request, mediaBase, segName)
 	}
 }
 
-func getMediaBase(mId int) string {
-	return fmt.Sprintf("%s/%d", folder, mId)
+func getMediaBase(mID int) string {
+	return fmt.Sprintf("%s/%d", folder, mID)
 }
 
 func serveHlsM3u8(w http.ResponseWriter, r *http.Request, mediaBase, m3u8Name string) {
@@ -124,4 +130,48 @@ func chunk(file string, resolution string, chunkSize string) error {
 	}
 
 	return nil
+}
+
+func getEnvPort() (string, error) {
+	v := os.Getenv("PORT")
+	if v == "" {
+		fmt.Println("PORT not provided. Stream will use 8000 instead")
+		return "8000", nil
+	}
+
+	if _, err := strconv.Atoi(v); err != nil {
+		return "8000", fmt.Errorf("Port %s is not valid. Stream will use 8000 instead", v)
+	}
+
+	return v, nil
+}
+
+func getEnvFile() (string, error) {
+	v := os.Getenv("FILE")
+	if v == "" {
+		fmt.Println("FILE not provided. Stream will reuse previous file instead")
+		return "", nil
+	}
+
+	return v, nil
+}
+
+func getEnvResolution() (string, error) {
+	v := os.Getenv("RESOLUTION")
+	if v == "" {
+		fmt.Println("RESOLUTION not provided. Stream will use 720x576 instead")
+		return "720x576", nil
+	}
+
+	return v, nil
+}
+
+func getEnvChunk() (string, error) {
+	v := os.Getenv("CHUNK")
+	if v == "" {
+		fmt.Println("CHUNK not provided. Stream will use 10 instead")
+		return "10", nil
+	}
+
+	return v, nil
 }
